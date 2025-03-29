eb4804f2b8949a9bd39652dbbeba36d74c27f2df
MAmmarRaza <me.raza@outlook.com>
2025-03-29 10:03:29 +0500

    ammar"

--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 docker run -d --name blog -p 80:5000 \
-  -e MONGO_URL="mongodb+srv://20ntucs1120:WU6TAoQiyFljPJxO@blog-cluster.bvblv9k.mongodb.net/blog" \
+  -e MONGO_URL="mongodb+srv://20ntucs1120:WU6TAoQiyFljPJxO@blog-cluster.bvblv8k.mongodb.net/blog" \
   mammarraza/blog:latest
 
