c8cb0ba562c872fc0793e78510c48f6c6730a6aa
MAmmarRaza <me.raza@outlook.com>
2025-03-29 11:24:29 +0500

    set

--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -2,6 +2,6 @@
 docker stop blog || true
 docker rm blog || true
 docker run -d --name blog -p 80:5000 \
-  -e MONGO_URL="mongodb+srv://20ntucs1120:WU6TAoQiyFljPJxO@blog-cluster.bvblv8k.mongodb.net/blog" \
+  -e MONGO_URL="mongodb+srv://20ntucs1120:WU6TAoQiyFljPJxO@blog-cluster.bvblv9k.mongodb.net/blog" \
   mammarraza/blog:latest
 
